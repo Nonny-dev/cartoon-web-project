c80662cbb20a31f191a744bb7a5b0e9bbda5b2e7
Nonny-dev <149983436+Nonny-dev@users.noreply.github.com>
2023-12-28 00:12:29 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
   <!-- header -->
-  <header class="p-3 text-bg-dark">
+  <header class="p-2 text-bg-dark">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="index.php" class="logo">
@@ -25,21 +25,22 @@
           <li><a href="#" class="nav-link px-2 text-white">การ์ตูนญี่ปุ่น</a></li>
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..."
-            aria-label="Search">
+        <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="ค้นหา..."
+            aria-label="ค้นหา">
         </form>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+          <button type="button" class="btn btn-outline-light me-2">เข้าสู่ระบบ</button>
+          <button type="button" class="btn btn-success">สมัครสมาชิก</button>
         </div>
       </div>
     </div>
   </header>
 
+
   <!-- slide -->
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide mt-4" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
         aria-current="true" aria-label="Slide 1"></button>
@@ -51,13 +52,13 @@
 
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="img/lookism.jpg" class="d-block w-100" >
+        <img src="img/banner1.png" class="d-block w-100">
       </div>
       <div class="carousel-item">
-        <img src="img/lookism.jpg" class="d-block w-100" >
+        <img src="img/banner1.png" class="d-block w-100">
       </div>
       <div class="carousel-item">
-        <img src="img/lookism.jpg" class="d-block w-100" >
+        <img src="img/banner1.png" class="d-block w-100">
       </div>
     </div>
 
@@ -73,7 +74,70 @@
     </button>
   </div>
 
+  <!--Video-->
+  <div class="d-flex flex-row justify-content-center align-items-center">
+  <div class="card aspect-ratio aspect-ratio-4x6" style="width: 18rem;">
+    <img src="https://animekimi.com/wp-content/uploads/2023/10/f4PmYfliarqfMeFNdnK6NZ6AU5i-185x278.jpg" class="card-img-top aspect-ratio-item" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Spy x Family Season 2 สปาย x แฟมิลี (ภาค2)</h5>
+      <p class="card-text">ตอนที่ 1-12 ซับไทย จบแล้ว</p>
+      <a href="#" class="btn btn-primary">ดูวิดีโอ</a>
+    </div>
+  </div>
 
+  <div class="card" style="width: 18rem;">
+    <img src="https://animekimi.com/wp-content/uploads/2023/10/3C7v4LRVJjQ4QRw4b88ozWMbK0z-185x278.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Kanojo mo Kanojo Season 2 จะคนไหนก็แฟนสาว (ภาค2)</h5>
+      <p class="card-text">ตอนที่ 1-12 ซับไทย จบแล้ว</p>
+      <a href="#" class="btn btn-primary">ดูวิดีโอ</a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="https://animekimi.com/wp-content/uploads/2023/10/2u7MIsViNcXYkiYdgoLCYRtVNXy-185x278.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Goblin Slayer II ก็อบลินสเลเยอร์ (ภาค2)</h5>
+      <p class="card-text">ตอนที่ 1-12 ซับไทย จบแล้ว</p>
+      <a href="#" class="btn btn-primary">ดูวิดีโอ</a>
+    </div>
+  </div>
+</div>
+
+<div class="d-flex flex-row justify-content-center align-items-center">
+  <div class="card aspect-ratio aspect-ratio-4x6" style="width: 18rem;">
+    <img src="https://animekimi.com/wp-content/uploads/2023/09/vs5IamKt9pXi0q3CRcDxIh5WWms-185x278.jpg" class="card-img-top aspect-ratio-item" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Sousou no Frieren คำอธิฐานในวันที่จากลา FRIEREN</h5>
+      <p class="card-text">ตอนที่ 1-17 ซับไทย และ พากย์ไทย ยังไม่จบ</p>
+      <a href="#" class="btn btn-primary">ดูวิดีโอ</a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="https://animekimi.com/wp-content/uploads/2023/10/138503l-185x278.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Keikenzumi na Kimi to หนุ่มซิงกับสาวฮอต เดตนี้จะรอดมั้ยนะ</h5>
+      <p class="card-text">ตอนที่ 1-12 ซับไทย จบแล้ว</p>
+      <a href="#" class="btn btn-primary">ดูวิดีโอ</a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="https://animekimi.com/wp-content/uploads/2023/04/hkzRP40C6yhBxZ63Z7pBHNhqdAs-1-185x278.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Boku no Kokoro no Yabai Yatsu เธอผู้อันตรายต่อใจผม เธอผู้อันตรายต่อใจผม</h5>
+      <p class="card-text">ตอนที่ 1-12+SP ซับไทย จบแล้ว</p>
+      <a href="#" class="btn btn-primary">ดูวิดีโอ</a>
+    </div>
+  </div>
+</div>
+
+
+  
+
+  <!--Footer-->
+  <?php include_once('components/footer.php') ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
